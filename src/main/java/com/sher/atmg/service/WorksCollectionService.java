0a74581612b471819d8ace7557628fcc3509821c
SHER <1160524900@qq.com>
2024-11-09 20:53:51 +0800

    添加忽略文件

--- src/main/java/com/sher/atmg/service/WorksCollectionService.java
+++
@@ -1,8 +0,0 @@
-package com.sher.atmg.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.sher.atmg.entity.WorksCollectionEntity;
-
-public interface WorksCollectionService extends IService<WorksCollectionEntity> {
-
-}
